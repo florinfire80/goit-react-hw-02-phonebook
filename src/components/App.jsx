cefc1961ceef198f32cfe80cb25c5329394ec423
Florin <florinfire80@yahoo.com>
2023-11-10 14:56:33 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      React homework template modified
+      <h1>test</h1>
     </div>
   );
 };
