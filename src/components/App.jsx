8a1567d96fd81e60ed7fa31002cef55ac940851d
Florin <florinfire80@yahoo.com>
2023-11-12 13:00:28 +0200

    commit ex 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,9 @@
+import React from 'react';
+import Phonebook from './Phonebook/Phonebook';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template modified
-      <h1>test</h1>
-    </div>
+    <main>
+      <Phonebook />
+    </main>
   );
 };
